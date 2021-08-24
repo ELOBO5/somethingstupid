297873321009b323a4ec05b61b52e51790a882d3
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-08-24 15:45:38 +0100

    ammended db schema

--- db/1_setup.sql
+++ db/1_setup.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS posts;
 
 CREATE TABLE posts (
     id serial PRIMARY KEY,
-    title varchar(40) NOT NULL,
-    name varchar(20) NOT NULL,
-    message varchar(250) NOT NULL
+    title varchar(50) NOT NULL,
+    name varchar(30) NOT NULL,
+    message varchar(400) NOT NULL
 );
