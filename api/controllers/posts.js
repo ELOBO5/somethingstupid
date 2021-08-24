b32a5372bf2e5a1ba9dc0fe0f49259027fd6751c
“ELOBO5” <elicialobo@hotmail.com>
2021-08-24 14:24:40 +0100

    add routes

---
+++ api/controllers/posts.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+const Post = require("../models/post");
+
+// get all posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.all;
+    res.json({ posts });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+// get post by id
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(parseInt(req.params.id));
+    if (id !== req.params.id) res.redirect("/");
+    res.json(post);
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+});
+
+// create post
+router.post("/", async (req, res) => {
+  try {
+    const post = await Post.create(
+      req.body.title,
+      req.body.name,
+      req.body.message
+    );
+    res.json(post);
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+});
+
+module.exports = router;
