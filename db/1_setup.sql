a1ef15e8f1a669c7803fecc781d75b3ad546b3e7
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-08-24 14:54:33 +0100

    added Dockerfile for db

--- db/1_setup.sql
+++ db/1_setup.sql
@@ -4,5 +4,5 @@ CREATE TABLE posts (
     id serial PRIMARY KEY,
     title varchar(40) NOT NULL,
     name varchar(20) NOT NULL,
-    message varchar(250) NOT NULL,
+    message varchar(250) NOT NULL
 );
