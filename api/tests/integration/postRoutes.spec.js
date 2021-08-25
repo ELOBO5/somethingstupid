ea650f9b760f44138bcc9d10199e75d255a03380
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-08-25 12:09:02 +0100

    added coverage script and post endpoint test

--- api/tests/integration/postRoutes.spec.js
+++ api/tests/integration/postRoutes.spec.js
@@ -1,4 +1,4 @@
-describe("post endpoints", () => {
+describe("endpoints", () => {
   let api;
   beforeEach(async () => {
     await resetTestDB();
@@ -15,10 +15,34 @@ describe("post endpoints", () => {
     api.close(done);
   });
 
-  it("should return a list of all posts in database", async () => {
+  it("/GET /posts should return a list of all posts in database", async () => {
     const res = await request(api).get("/posts");
-    console.log(res);
     expect(res.statusCode).toEqual(200);
     expect(res.body.posts.length).toEqual(3);
   });
+
+  it("/GET /posts/:query should return a list of all posts containing query as substring", async () => {
+    const res = await request(api).get("/posts/third");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.posts.length).toEqual(1);
+  });
+
+  it("/GET /posts/:query should return a list of all posts containing query as substring", async () => {
+    const res = await request(api).get("/posts/fourth");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.posts.length).toEqual(0);
+  });
+
+  it("/POST /posts should add a post to the database and return 201", async () => {
+    const testData = {
+      title: 'Title 5',
+      name: 'User 5',
+      message: 'fifth'
+    }
+
+    await request(api).post("/posts")
+      .send(testData)
+      .expect(201)
+  });
+
 });
